0099466807f9215390b13ddb1e174f0535182df7
Julie Belleville <35176177+jbellevi@users.noreply.github.com>
2022-05-16 09:22:16 -0700

    circuit variable name refactors

--- src/ops.py
+++ src/ops.py
@@ -6,9 +6,9 @@ class Operation:
     """
 
     """
-    def __init__(self, qudits=tuple(), cbits=tuple()):
-        self.qudits = qudits
-        self.cbits = cbits
+    def __init__(self, q_registers=tuple(), c_registers=tuple()):
+        self.q_registers = q_registers
+        self.c_registers = c_registers
 
 
 class Input(Operation):

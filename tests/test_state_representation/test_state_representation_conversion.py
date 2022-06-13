d97288d72eee843cdfb5c6541086f1c9d6836f54
LucRob <luc.robichaud@quantumbridgetech.com>
2022-06-13 10:21:30 -0400

    Added 3 new tests, two on density to graph and one from graph to density.

--- tests/test_state_representation/test_state_representation_conversion.py
+++ tests/test_state_representation/test_state_representation_conversion.py
@@ -93,3 +93,54 @@ def test_stabilizer_and_density_conversion2():
     assert reduce(lambda x, y: x and y,
                   map(lambda a, b: a == b, stabilizer1,  stabilizer2), True)
 
+
+def test_density_to_graph_linear_cluster_3():
+    #%% Test density to graph for N=3 linear cluster state
+
+    #Construct density matrix
+    rho=np.ones([8,8]); 
+    inds=[3,6]
+    for i in inds:
+        rho[:,i]=-1*rho[:,i]; rho[i,:]=-1*rho[i,:]
+    rho=rho/rho.trace()
+
+    #Get graph
+    graph=sconverter.density_to_graph(rho, threshold=0.1)
+    assert(np.isreal(graph).all())
+    assert((graph>=0).all())
+    
+    #Compare adjancency matrix to exprected expression
+    Adj=np.zeros([3,3]); Adj[0,1]=1; Adj[1,2]=1; Adj=Adj+Adj.transpose();
+    assert((graph==Adj).all())
+
+
+def test_density_to_graph_Triangle():
+    #%% Test density to graph for triangle state
+
+    #Construct density matrix
+    rho=np.ones([8,8]); 
+    inds=[3,5,6,7]
+    for i in inds:
+        rho[:,i]=-1*rho[:,i]; rho[i,:]=-1*rho[i,:]
+    rho=rho/rho.trace()
+
+    #Get graph
+    graph=sconverter.density_to_graph(rho, threshold=0.1)
+    assert(np.isreal(graph).all()) #Matrix should be real
+    assert((graph>=0).all()) #Matrix should consist of elements greater or equal to zero
+    
+    #Compare calculated adjancency matrix to exprected expression
+    Adj=np.zeros([3,3]); Adj[0,1]=1; Adj[0,2]=1; Adj[1,2]=1; Adj=Adj+Adj.transpose();
+    assert((graph==Adj).all()) 
+    
+    
+def test_graph_to_density_linear_cluster_3():
+    #Test density matrix obtained from graph state for the N=3 linear cluster state
+    
+    #Graph for N=3 linear cluster state
+    graph = nx.Graph([(1, 2), (2, 3)])    
+
+    #Get density matrix
+    rho=sconverter.graph_to_density(graph)
+    assert(np.size(rho)==8**2) #test expected size
+    assert((np.conjugate(rho.transpose())==rho).all()) #Test hermiticity

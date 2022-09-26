255b17b80a515c0f31d0e428d9c9536475b162bb
aayush3416 <87783633+aayush3416@users.noreply.github.com>
2022-09-26 11:28:58 -0400

    remove code that broke ciruict.draw_circuit()

--- src/visualizers/openqasm_visualization.py
+++ src/visualizers/openqasm_visualization.py
@@ -34,7 +34,6 @@ def draw_openqasm(qasm, show=False, ax=None, display_text=None):
     if ax is None:
         fig, ax = plt.subplots()
         qc.draw(output="mpl", ax=ax, plot_barriers=False, style=style)
-        plt.close(fig)
     else:
         fig = qc.draw(output="mpl", ax=ax, plot_barriers=False, style=style)
     if show:

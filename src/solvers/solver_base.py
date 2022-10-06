7ef1cf40d848b4e116a9d9b47b0dcaae31b37c8d
aayush3416 <87783633+aayush3416@users.noreply.github.com>
2022-10-06 15:39:10 -0400

    change variable name

--- src/solvers/solver_base.py
+++ src/solvers/solver_base.py
@@ -101,18 +101,18 @@ class RandomSearchSolver(SolverBase):
         super().__init__(target, metric, compiler, circuit, io)
 
         # hof stores the best circuits and their scores in the form of: (scores, circuits)
-        self._n_hof = kwargs.get("n_hof", 10)
+        self.n_hof = kwargs.get("n_hof", 10)
         self.n_pop = kwargs.get("n_pop", 10)
         self.n_stop = kwargs.get("n_stop", 10)
 
-        self.hof = [(np.inf, None) for _ in range(self._n_hof)]
+        self.hof = [(np.inf, None) for _ in range(self.n_hof)]
         self.trans_probs = {None: None}
         self.transformations = list(self.trans_probs.keys())
         self.logs = {"population": [], "hof": []}
 
     @property
     def n_hof(self):
-        return self._n_hof
+        return self.n_hof
 
     def update_hof(self, population):
         """
@@ -166,3 +166,7 @@ class RandomSearchSolver(SolverBase):
         raise NotImplementedError(
             "Base Solver class, solver method is not implemented."
         )
+
+    @n_hof.setter
+    def n_hof(self, value):
+        self._n_hof = value

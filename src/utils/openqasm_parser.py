bd8bbf403b1f926f245e8fac70f9a703b448c016
kvktran2812 <kvktran@gmail.com>
2022-12-13 21:16:06 -0500

    change static methods to class methods and handle params in gates

--- src/utils/openqasm_parser.py
+++ src/utils/openqasm_parser.py
@@ -6,14 +6,15 @@ class OpenQASMParser:
     """
     This parser just use the Qiskit parser to parse the openqasm string, but the information is more readable.
     """
+
     def __init__(self, openqasm_str: str):
         self.openqasm = openqasm_str
 
         self.ast = {
             "def": {
-                "qreg": [],
-                "creg": [],
-                "gate": [],
+                "qreg": {},
+                "creg": {},
+                "gate": {},
             },
             "ops": []
         }
@@ -25,49 +26,66 @@ class OpenQASMParser:
             # print(node.type)
             # add to def
             if node.type == 'qreg':
-                self.ast["def"]["qreg"].append(self.parse_qreg_info(node))
+                self._parse_qreg(node)
             if node.type == 'creg':
-                self.ast["def"]["creg"].append(self.parse_creg_info(node))
+                self._parse_creg(node)
+            if node.type == 'gate':
+                self._parse_gate(node)
 
             # add to ops
             if node.type == 'barrier':
-                self.ast["ops"].append(self.parse_barrier(node))
+                self.ast["ops"].append(self._parse_barrier(node))
             if node.type == 'reset':
-                self.ast["ops"].append(self.parse_reset(node))
+                self.ast["ops"].append(self._parse_reset(node))
             if node.type == 'measure':
-                self.ast["ops"].append(self.parse_measure(node))
+                self.ast["ops"].append(self._parse_measure(node))
             if node.type == 'cnot':
-                self.ast["ops"].append(self.parse_cnot(node))
+                self.ast["ops"].append(self._parse_cnot(node))
             if node.type == 'custom_unitary':
-                self.ast["ops"].append(self.parse_custom_unitary(node))
+                self.ast["ops"].append(self._parse_custom_unitary(node))
             if node.type == 'if':
-                self.ast["ops"].append(self.parse_if(node))
+                self.ast["ops"].append(self._parse_if(node))
 
-    @staticmethod
-    def parse_qreg_info(qreg_node):
+    def _parse_qreg(self, qreg_node):
         qreg_info = {
-            'name': qreg_node.name,
             'index': qreg_node.index,
         }
+        self.ast['def']['qreg'][qreg_node.name] = qreg_info
 
         return qreg_info
 
-    @staticmethod
-    def parse_gate(gate_node):
+    # TODO: Maybe add parse body, because in the gate body there is a list of statement
+    def _parse_gate(self, gate_node):
         gate_info = {
+            'type': gate_node.type,
+            'params': [],
+            'qargs': [],
         }
+
+        # Get params and qargs
+        # Go to 1st children node, nodes that not is_bit is param, node that is_bit is qargs
+        for node in gate_node.children[1].children:
+            if node.is_bit:
+                gate_info['qargs'].append(node.name)
+            else:
+                gate_info['params'].append(node.name)
+        self.ast['def']['gate'][gate_node.name] = gate_info
+
+        # some qarg is in the 2nd children node, don't know why
+        for node in gate_node.children[2].children:
+            if node.type == 'id':
+                gate_info['qargs'].append(node.name)
+
         return gate_info
 
-    @staticmethod
-    def parse_creg_info(creg_node):
-        qreg_info = {
-            'name': creg_node.name,
+    def _parse_creg(self, creg_node):
+        creg_info = {
             'index': creg_node.index,
         }
-        return qreg_info
+        self.ast['def']['creg'][creg_node.name] = creg_info
+        return creg_info
 
-    @staticmethod
-    def parse_barrier(barrier_node):
+    def _parse_barrier(self, barrier_node):
         barrier_info = {
             'type': barrier_node.type,
             "qreg": []
@@ -77,8 +95,7 @@ class OpenQASMParser:
                 barrier_info['qreg'].append(children_node.name)
         return barrier_info
 
-    @staticmethod
-    def parse_reset(reset_node):
+    def _parse_reset(self, reset_node):
         reset_info = {
             'type': reset_node.type,
         }
@@ -88,42 +105,41 @@ class OpenQASMParser:
                 reset_info["index"] = node.index
         return reset_info
 
-    @staticmethod
-    def parse_custom_unitary(custom_unitary_node):
-        def parse_expression(expression_node):
-            expression_info = {}
-            if expression_node.type == 'binop':
-                for binop_node in expression_node.children:
-                    if binop_node.type == 'operator':
-                        expression_info['operator'] = binop_node.value
-                    if binop_node.type == 'real':
-                        expression_info['real'] = binop_node.value
-                    if binop_node.type == 'int':
-                        expression_info['int'] = binop_node.value
-            else:
-                expression_info[expression_node.type] = expression_node.value
-            return expression_info
+    def _parse_expression(self, expression_node):
+        expression_info = {}
+        if expression_node.type == 'binop':
+            for binop_node in expression_node.children:
+                if binop_node.type == 'operator':
+                    expression_info['operator'] = binop_node.value
+                if binop_node.type == 'real':
+                    expression_info['real'] = binop_node.value
+                if binop_node.type == 'int':
+                    expression_info['int'] = binop_node.value
+        else:
+            expression_info[expression_node.type] = expression_node.value
+        return expression_info
 
+    def _parse_custom_unitary(self, custom_unitary_node):
         custom_unitary_info = {
             'type': custom_unitary_node.type,
             'name': custom_unitary_node.name,
-            'params': [],
+            'params': {},
             'qargs': [],
         }
+        params_mapping = self.ast['def']['gate'][custom_unitary_node.name]['params']
 
         for node in custom_unitary_node.children:
+            # get params
             if node.type == 'expression_list':
-                for exp_node in node.children:
-                    custom_unitary_info['params'].append(parse_expression(exp_node))
+                for i, exp_node in enumerate(node.children):
+                    custom_unitary_info['params'][params_mapping[i]] = self._parse_expression(exp_node)
             if node.type == 'primary_list':
                 for p_node in node.children:
                     qarg = (p_node.name, p_node.index)
                     custom_unitary_info['qargs'].append(qarg)
-
         return custom_unitary_info
 
-    @staticmethod
-    def parse_measure(measure_node):
+    def _parse_measure(self, measure_node):
         measure_info = {
             'type': measure_node.type,
         }
@@ -145,8 +161,7 @@ class OpenQASMParser:
 
         return measure_info
 
-    @staticmethod
-    def parse_cnot(cnot_node):
+    def _parse_cnot(self, cnot_node):
         cnot_info = {
             'type': cnot_node.type,
             'control': {
@@ -160,13 +175,16 @@ class OpenQASMParser:
         }
         return cnot_info
 
-    @staticmethod
-    def parse_if(if_node):
+    def _parse_if(self, if_node):
         if_info = {
-            type: if_node.type,
+            'type': if_node.type,
         }
-
-        print(vars(if_node))
-
+        for node in if_node.children:
+            if node.type == 'custom_unitary':
+                custom_unitary = self._parse_custom_unitary(node)
+                if_info['custom_unitary'] = custom_unitary
+            if type == 'id':
+                if_info['creg'] = node.name
+            if type == 'int':
+                if_info['value'] = node.value
         return if_info
-
